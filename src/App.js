c38118ba8e6e85976fc2f29b8b32f3b99b229fc8
Jay Tyagi <94462361+JayTyagi01@users.noreply.github.com>
2024-02-03 00:00:10 +0530

    error occured whole tracker is completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import { BrowserRouter,Route } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
+import Homepage from "./Pages/HomePage";
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
 import Header from "./Components/Header";
-import Homepage from "./Pages/Homepage";
-import Coinpage from "./Pages/Coinpage";
-import {makeStyles} from "@material-ui/core"
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
-  const useStyles = makeStyles(() => ({
-    App:{
-      backgroundColor:"#14161a",
-      color:"white",
-      minHeight:"100vh",
-    }
-  }));
-  const classes =useStyles()
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
-      <div className="classses.App">
+      <div className={classes.App}>
         <Header />
-        <Route path="/" Component={Homepage} exact/>
-        <Route path="/coins/:id" Component={Coinpage}/>
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
       </div>
     </BrowserRouter>
   );
